64201a35cf23496f3abc218c7083ca292c269206
João Gabriel <joaogabrielmoura14@gmail.com>
2026-09-04 08:26:42 -0300

    fix: correção da identação

--- main.py
+++ main.py
@@ -1,99 +1,96 @@
-    from datetime import timedelta
+from datetime import timedelta
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+import models
+import schemas
+from database import engine, get_db
+from auth import ACCESS_TOKEN_EXPIRE_MINUTES, criar_token_acesso, hash_senha, autenticar_usuario, get_current_user
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-    from fastapi import FastAPI, Depends, HTTPException, status
-    from sqlalchemy.orm import Session
-    from typing import List
-    import models
-    import schemas
-    from database import engine, get_db
-    from auth import ACCESS_TOKEN_EXPIRE_MINUTES, ACCESS_TOKEN_EXPIRE_MINUTES, criar_token_acesso, hash_senha, autenticar_usuario, get_current_user
-    from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+models.Base.metadata.create_all(bind=engine)
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+app = FastAPI()
 
-    models.Base.metadata.create_all(bind=engine)
-
-    oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-    app = FastAPI()
-
-    @app.get("/")
-    def root():
-        return {"message": "API rodando com PostgreSQL"}
+@app.get("/")
+def root():
+    return {"message": "API rodando com PostgreSQL"}
 
 
 
-    @app.post("/register/", response_model=schemas.UsuarioResponse)
-    def criar_usuario(usuario: schemas.UsuarioCreate, db: Session = Depends(get_db)):
-        db_usuario = models.Usuario(
-            nome=usuario.nome,
-            email=usuario.email,
-            senha=hash_senha(usuario.senha),
-            telefone=usuario.telefone
+@app.post("/register/", response_model=schemas.UsuarioResponse)
+def criar_usuario(usuario: schemas.UsuarioCreate, db: Session = Depends(get_db)):
+    db_usuario = models.Usuario(
+    nome=usuario.nome,
+    email=usuario.email,
+    senha=hash_senha(usuario.senha),
+    telefone=usuario.telefone
+)
+    db.add(db_usuario)
+    db.commit()
+    db.refresh(db_usuario)
+    return db_usuario
+
+@app.get("/usuarios/", response_model=List[schemas.UsuarioResponse])
+def listar_usuarios(db: Session = Depends(get_db)):
+    return db.query(models.Usuario).all()
+
+
+
+@app.post("/login/", response_model=schemas.TokenResponse)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    usuario = autenticar_usuario(db, form_data.username, form_data.password)
+    if not usuario:
+        raise HTTPException(status_code=400, detail="Usuário ou senha incorretos")
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = criar_token_acesso(
+        data={"sub": usuario.email},
+        expires_delta=access_token_expires
         )
-        db.add(db_usuario)
-        db.commit()
-        db.refresh(db_usuario)
-        return db_usuario
-
-    @app.get("/usuarios/", response_model=List[schemas.UsuarioResponse])
-    def listar_usuarios(db: Session = Depends(get_db)):
-        return db.query(models.Usuario).all()
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 
-    @app.post("/login/", response_model=schemas.TokenResponse)
-    def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-        usuario = autenticar_usuario(db, form_data.username, form_data.password)
-        if not usuario:
-            raise HTTPException(status_code=400, detail="Usuário ou senha incorretos")
+@app.get("/me", response_model=schemas.UsuarioResponse)
+async def get_me(usuario: models.Usuario = Depends(get_current_user)):
+    return usuario
 
-        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-        access_token = criar_token_acesso(
-            data={"sub": usuario.email},
-            expires_delta=access_token_expires
+
+@app.post("/tickets/", response_model=schemas.TicketResponse)
+def criar_ticket(ticket: schemas.TicketCreate, db: Session = Depends(get_db)):
+    db_ticket = models.Ticket(
+        titulo=ticket.titulo,
+        descricao=ticket.descricao,
+        prioridade=ticket.prioridade,
+        categoria=ticket.categoria,
+        anexo_url=ticket.anexo_url,
+        status="aberto", 
+        criado_por=1,     
+        criador_nome="João" 
         )
-        return {"access_token": access_token, "token_type": "bearer"}
+    db.add(db_ticket)
+    db.commit()
+    db.refresh(db_ticket)
+    return db_ticket
 
 
-
-    @app.get("/me", response_model=schemas.UsuarioResponse)
-    async def get_me(usuario: models.Usuario = Depends(get_current_user)):
-        return usuario
-
-
-    @app.post("/tickets/", response_model=schemas.TicketResponse)
-    def criar_ticket(ticket: schemas.TicketCreate, db: Session = Depends(get_db)):
-        db_ticket = models.Ticket(
-            titulo=ticket.titulo,
-            descricao=ticket.descricao,
-            prioridade=ticket.prioridade,
-            categoria=ticket.categoria,
-            anexo_url=ticket.anexo_url,
-            status="aberto", 
-            criado_por=1,     
-            criador_nome="João" 
-        )
-        db.add(db_ticket)
-        db.commit()
-        db.refresh(db_ticket)
-        return db_ticket
+@app.get('/listar/', response_model=list[schemas.TicketResponse])
+def listar_ticket(db: Session = Depends(get_db)):
+    tickets = db.query(models.Ticket).all()
+    return tickets
 
 
-    @app.get('/listar/', response_model=list[schemas.TicketResponse])
-    def listar_ticket(db: Session = Depends(get_db)):
-        tickets = db.query(models.Ticket).all()
-        return tickets
+@app.post("/produtos/", response_model=schemas.ProdutoResponse)
+def criar_produto(produto: schemas.ProdutoCreate, db: Session = Depends(get_db)):
+    db_produto = models.Produto(**produto.model_dump())
+    db.add(db_produto)
+    db.commit()
+    db.refresh(db_produto)
+    return db_produto
 
-
-    @app.post("/produtos/", response_model=schemas.ProdutoResponse)
-    def criar_produto(produto: schemas.ProdutoCreate, db: Session = Depends(get_db)):
-        db_produto = models.Produto(**produto.model_dump())
-        db.add(db_produto)
-        db.commit()
-        db.refresh(db_produto)
-        return db_produto
-
-    @app.get("/produtos/", response_model=List[schemas.ProdutoResponse])
-    def listar_produtos(db: Session = Depends(get_db)):
-        return db.query(models.Produto).all()
+@app.get("/produtos/", response_model=List[schemas.ProdutoResponse])
+def listar_produtos(db: Session = Depends(get_db)):
+    return db.query(models.Produto).all()
